ee403e56340126a1ea5631b77ecd1cd833af7fc2
DaniloDegiglio-22 <danilodegiglio973@gmail.com>
2026-01-20 16:49:14 +0100

    lezione2

---
+++ starway.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# NOTE: it is recommended to use this even if you don't understand the following code.
+
+import sys
+# uncomment the two following lines if you want to read/write from files
+#sys.stdin = open('starway_input1.txt')
+#sys.stdout = open('starway_output.txt', 'w')
+
+N = int(input().strip())
+
+B = 0
+
+
+# INSERT YOUR CODE HERE
+for i in range(1,N+1):
+    B+= i
+
+print(B)
+
+sys.stdout.close()
