1da91cbd7f6b91a9e250e996aa955b3870379ff9
DaniloDegiglio-22 <danilodegiglio973@gmail.com>
2026-02-17 16:53:35 +0100

    soluzione

--- isogram.py
+++ isogram.py
@@ -1,36 +1,37 @@
+#!/usr/bin/env python3
 # NOTE: it is recommended to use this even if you don't understand the following code.
-import sys
-sys.stdin = open('isogram_input0.txt', 'r')
-sys.stdout = open('output.txt', 'w')
 
+import sys
+
+#sys.stdin = open('isogram_input0.txt', 'r')
+#sys.stdout = open('output.txt', 'w')
 # input data
 N = int(input().strip())
-risposta = 0 
+risposta = 0
 
 for i in range(N):
     S = input().strip()
 
-    # insert your code here
-    pulita= ""
-    for i in range(len(S)):
+    pulita = ""
+    for i in range(0, len(S)):
         if S[i].isalpha():
-            clean += S[i]
+            pulita += S[i] 
 
-pulita = pulita.lower()
+    pulita = pulita.lower()
 
-isogram = True
-contatori = {}
-for c in clean:
-    if c not in pulita:
-        contatori[c] = 1
-    else:
-        contatori[c] += 1
-    if contatori[c] > 2:
-        isogram = False
-        break
-if isogram:
-    risposta += 1
+    isogram = True
+    contatori = {}
+    for c in pulita:
+        if c not in contatori:
+            contatori[c] = 1
+        else:
+            contatori[c] += 1
 
+        if contatori[c] > 2:
+            isogram = False
+            break
 
+    if isogram:
+        risposta +=1
 
 print(risposta)  # print the result
